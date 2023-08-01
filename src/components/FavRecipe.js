f963a652d0f8d1164dc469cea65cc4d3c318f9df
ninamuz <gia.muzzio@gmail.com>
2023-08-01 18:27:15 -0400

    Showing recipe details from favorite recipes page

--- src/components/FavRecipe.js
+++ src/components/FavRecipe.js
@@ -1,22 +1,33 @@
 import FavContext from "../context/favorites";
+import RecipesContext from "../context/recipes";
 import React, { useContext } from 'react';
 import '../styles.css';
 
 export default function FavRecipe({ recipe }) {
 
   const { removeFromFavorites } = useContext(FavContext);
+  const { setClickedRecipe, setView } = useContext(RecipesContext);
 
-  const handleRemoveRecipe = (e) => {
+  const handleRemoveRecipe = () => {
     removeFromFavorites(recipe);
   };
 
+  const handleGoToDetails = () => {
+    console.log('Sending recipe: ', recipe);
+    setClickedRecipe(recipe);
+    setView(true);
+  };
+
   return (
-    <div className="favorites">
+    <div className="favorite">
+      <h2>{recipe.label}</h2>
       <div className='fav-header'>
-        <h2>{recipe.label}</h2>
-        <button value={recipe} onClick={handleRemoveRecipe}>
+        <span value={recipe} onClick={handleRemoveRecipe}>
           <ion-icon name="trash-outline"></ion-icon>
-        </button>
+        </span>
+        <span value={recipe} onClick={handleGoToDetails}>
+          <ion-icon name="eye-outline"></ion-icon>
+        </span>
       </div>
     </div>
   )
--- src/components/MyFavRecipes.js
+++ src/components/MyFavRecipes.js
@@ -1,11 +1,14 @@
 import FavContext from "../context/favorites";
-import React, { useContext } from 'react';
+import RecipesContext from "../context/recipes";
+import React, { useContext, useState } from 'react';
 import FavRecipe from "./FavRecipe";
+import RecipeDetails from "./RecipeDetails";
 import '../styles.css';
 
 function MyFavRecipes() {
 
   const { favorites } = useContext(FavContext);
+  const { clickedRecipe, view } = useContext(RecipesContext);
 
   const renderedFavs = favorites.map(recipe => {
     if (recipe.label) {
@@ -18,10 +21,28 @@ function MyFavRecipes() {
     return null;
   });
 
+  const showRecipe = () => {
+    if (view == true) {
+      return <div>
+        <RecipeDetails currentPage='MyFavRecipes'/>
+      </div>
+    } else {
+      return <div className="waiting-screen">Click on a recipe to show details</div>
+    }
+  };
+
   return (
-    <div className="favorites">
-      <h1>My Fav Recipes</h1>
-      <ul>{renderedFavs}</ul>
+    <div className="favorites-container">
+
+      <div className="favorites-list">
+        <h2>My Favorite Recipes</h2>
+        <ul>{renderedFavs}</ul>
+      </div>
+
+      <div className="fav-details">
+        {showRecipe()}
+      </div>
+
     </div>
   )
 }
